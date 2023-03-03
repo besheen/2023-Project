0793c26e0acc8f6cc4049e9d181e92969893e8c2
zhongbisheng <bisheng.zhong@gmail.com>
2023-03-03 09:41:04 +0800

    Add ReactiveCocoa 教程.md

--- RAC+MVVM/RAC+MVVM/ViewController.m
+++ RAC+MVVM/RAC+MVVM/ViewController.m
@@ -7,16 +7,69 @@
 
 #import "ViewController.h"
 
+@import ReactiveObjC;
+@import Masonry;
+
 @interface ViewController ()
 
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UIButton *button;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.view addSubview:self.textField];
+    [self.view addSubview:self.button];
+    [_textField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(64);
+        make.left.equalTo(self.view).offset(44);
+        make.right.equalTo(self.view).offset(-44);
+        make.height.mas_equalTo(64);
+    }];
+    [_button mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.textField.mas_bottom).offset(100);
+        make.left.equalTo(self.view).offset(44);
+        make.right.equalTo(self.view).offset(-44);
+        make.height.mas_equalTo(54);
+    }];
+    
+    RACSignal *validEmailSignal = [self.textField.rac_textSignal map:^id _Nullable(NSString * _Nullable value) {
+        return @([value rangeOfString:@"@"].location != NSNotFound);
+    }];
+    RAC(self.button, enabled) = validEmailSignal;
+    RAC(self.textField, textColor) = [validEmailSignal map:^id _Nullable(id  _Nullable value) {
+        return [value boolValue] ? [UIColor greenColor] : [UIColor redColor];
+    }];
 }
 
+- (void)commitAction:(UIButton *)sender {
+    NSLog(@"commit action");
+}
 
+- (UITextField *)textField {
+    if (!_textField) {
+        _textField = [[UITextField alloc] init];
+        _textField.layer.cornerRadius = 4;
+        _textField.layer.borderWidth = 0.5;
+        _textField.layer.borderColor = [UIColor blackColor].CGColor;
+        _textField.placeholder = @"请输入正确邮箱";
+    }
+    return _textField;
+}
+
+- (UIButton *)button {
+    if (!_button) {
+        _button = [UIButton buttonWithType:UIButtonTypeCustom];
+        _button.titleLabel.font = [UIFont systemFontOfSize:16];
+        _button.backgroundColor = [UIColor grayColor];
+        [_button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_button setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
+        [_button setTitle:@"提交" forState:UIControlStateNormal];
+        [_button addTarget:self action:@selector(commitAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _button;
+}
 @end
--- ReactiveCocoa 教程.md
+++ ReactiveCocoa 教程.md
@@ -6,6 +6,67 @@ RACSignal（信号类），只是表示当数据变化时，信号内部会发�
 
 默认一个信号都是冷信号，就算是值改变了，但你没有订阅这个信号的话它也不会触发的，只有订阅了这个信号，才会变成热信号，值改变了才会触发。
 
+使用步骤：
+
+1、创建信号
+
+```objective-c
++ (RACSignal *)createSignal:(RACDisposable * (^)(id<RACSubscriber> subscriber))didSubscribe
+```
+
+2、订阅信号（才会激活信号）
+
+```objective-c
+- (RACDisposable *)subscribeNext:(void (^)(id x))nextBlock
+```
+
+3、发送信号
+
+```objective-c
+- (void)sendNext:(id)value
+```
+
+eg：
+
+```objective-c
+	// 1.创建信号
+    // didSubscribe调用：只要一个信号被订阅了就会调用
+    // didSubscribe作用：发送数据
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        // 3.发送信号
+        [subscriber sendNext:@"111"];
+        // 如果不再发送数据，最好发送信号完成，内部会自动调用 [RACDisposable disposable] 取消订阅信号
+        [subscriber sendCompleted];
+        
+        // block调用：当信号发送完成或者发送错误时，就会自动执行这个block，取消订阅信号
+        return [RACDisposable disposableWithBlock:^{
+            // 执行完block后，当前信号就不再被订阅了
+            NSLog(@"信号被销毁");
+        }];
+    }];
+    // 2.订阅信号
+    // nextBlock调用：每当有信号发出数据，就会调用block
+    // nextBlock作用：处理数据，展示到 UI 上面
+    [signal subscribeNext:^(id  _Nullable x) {
+        // x：信号发送的内容
+        NSLog(@"接收数据的值：%@", x);
+    }];
+```
+
+> **RACSubscriber**
+>
+> 表示订阅者的意思，用于发送信号，这是一个协议，不是一个类，只要遵守这个协议，并且实现方法才能成为订阅者。
+>
+> 通过 `create` 创建的信号，都有一个订阅者，帮助他发送数据
+>
+> **RACDisposable**
+>
+> 用于取消订阅或者清理资源，当信号发送完成或者发送错误的时候，就会自动触发它。
+>
+> 使用场景：不想监听某个信号时，可以通过它主动取消订阅信号。
+
+
+
 
 
 #### RACSubject
